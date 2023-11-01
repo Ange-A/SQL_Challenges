b8fb1f67df34b5763eefa5405948bbab8e7692ef
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-11-01 20:15:54 +0200

    sql

--- Japanese_city.sql
+++ Japanese_city.sql
@@ -1,4 +1,5 @@
-/* Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+/* Query all attributes of every Japanese city in the CITY table.
+he COUNTRYCODE for Japan is JPN.
 
 */
 
